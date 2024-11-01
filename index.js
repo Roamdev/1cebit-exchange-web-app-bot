3254f1407ad6ca468f4a0f949a9312f10e85cf18
Roamdev <aroundthefog@gmail.com>
2024-11-01 19:21:40 +0400

    feat!: create index.html. create response to command /start and add button

--- index.js
+++ index.js
@@ -10,7 +10,23 @@ const bot = new TelegramBot(process.env.TOKEN, {
 bot.on("polling_error", err => console.log(err.data.error.message));
 
 bot.on('text', async msg => {
-
     await bot.sendMessage(msg.chat.id, msg.text);
-
 })
+
+bot.onText(/\/start/, async (msg) => {
+    try {
+        const keyboard = {
+            reply_markup: {
+                keyboard: [[{
+                    text: 'Открыть приложение',
+                    web_app: {url: 'https://github.com/Roamdev/1cebitExchangeWebApp'} 
+                }]],
+                resize_keyboard: true
+            }
+        };
+        
+        await bot.sendMessage(msg.chat.id, 'Привет! Нажми на кнопку, чтобы открыть приложение', keyboard);
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+});
